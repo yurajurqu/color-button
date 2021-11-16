853d0ed4f71b3240f5f90fb4be9b1897c591a98b
obarboza <obarboza@pe.ibm.com>
2021-11-16 18:40:52 -0500

    button to change colors

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color', async  () => {
+  render(<App></App>);
+  const colorButton =  await screen.findByRole('button', { name: /change to blue/i });
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
 });
+test('button turns blue when clicked', async () => {
+  render(<App></App>);
+  const colorButton = await screen.findByRole('button', { name: /change to blue/i });
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+  expect(colorButton.textContent).toMatch(/change to red/i);
+});
+test('button has correct initial text', () => {
+  
+});
+
